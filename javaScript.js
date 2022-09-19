8be0cfc92635202a2771ba305898b334046569c2
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-19 10:09:08 +0300

    se - min - hours revise

--- javaScript.js
+++ javaScript.js
@@ -7,6 +7,11 @@ let second = new Date().getSeconds();
 let minute = new Date().getMinutes();
 let hour = new Date().getHours();
 
+sec.style.transform = `rotate(${180 + (second * 6)}deg)`
+min.style.transform = `rotate(${180 + (minute * 6)}deg)`
+hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
+
+
 console.log(second, minute, hour);
 }
 
