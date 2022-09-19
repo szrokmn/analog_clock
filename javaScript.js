c2e11444c6567a16729de69195548417c9ea691f
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-19 10:02:41 +0300

    javascript continue

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,16 @@
+const sec = document.querySelector(".secs");
+const min = document.querySelector(".mins");
+const hours = document.querySelector(".hours");
 
+function tiktak() {
+let second = new Date().getSeconds();
+let minute = new Date().getMinutes();
+let hour = new Date().getHours();
+
+console.log(second, minute, hour);
+}
+
+setInterval(tiktak, 1000);
 
 
 
